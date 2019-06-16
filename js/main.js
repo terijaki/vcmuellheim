2669d3555a4fa7e4f7d10c86ddce829daf23c31d
Bjorn <bjornkohnen@gmail.com>
2019-06-16 15:53:13 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 
   // Header fixed and Back to top button
   $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
+    if (location.pathname == "/" AND $(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
       $('#header').addClass('header-fixed');
     } else {
