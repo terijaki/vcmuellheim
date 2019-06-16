742ad32667db0a2f2063641e134ff17b9a4aa915
Bjorn <bjornkohnen@gmail.com>
2019-06-16 15:40:55 +0200

    Create dynamicscroll.js

---
+++ js/dynamicscroll.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($) {
+
+  // Header fixed and Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+      $('#header').addClass('header-fixed');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+      $('#header').removeClass('header-fixed');
+    }
+  });
+
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
+    $('#header').addClass('header-fixed');
+  }
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+  
+  }
