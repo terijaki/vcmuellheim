444746c65b328fe1627beb8c197c7d96057172af
Bjorn <bjornkohnen@gmail.com>
2019-06-16 15:46:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
 jQuery(document).ready(function($) {
 
   // Header fixed and Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-      $('#header').addClass('header-fixed');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-      $('#header').removeClass('header-fixed');
-    }
-  });
+  // ## Include dynamicscroll.js to transition <intro> into the navbar 
 
   if ($(this).scrollTop() > 100) {
     $('.back-to-top').fadeIn('slow');
