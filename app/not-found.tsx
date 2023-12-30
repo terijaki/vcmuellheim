42c4763f88425c94ecf78dba4e2769e777d4401e
Terijaki <590522+terijaki@users.noreply.github.com>
2023-12-30 19:29:18 +0100

    themed

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,21 @@
 import Link from "next/link";
+import { FaTriangleExclamation as IconError } from "react-icons/fa6";
 
 export default function NotFound() {
 	return (
-		<div className="col-center-content bg-emerald-700">
-			<p>Die angefragte Seite kann nicht gefunden werden.</p>
-			<Link href="/">zur Startsteite</Link>
+		<div className="col-center-content flex flex-col gap-4 my-6 justify-center items-center">
+			<p>
+				<IconError className="text-5xl text-onyx" />
+			</p>
+			<p>Der angefragte Bereich kann nicht gefunden werden.</p>
+			<p>
+				<Link
+					href="/"
+					className="button"
+				>
+					zur Startsteite
+				</Link>
+			</p>
 		</div>
 	);
 }
