852de5748d5e37d4b2c4823bfd17daf5977cdf44
Terijaki <590522+terijaki@users.noreply.github.com>
2023-12-31 15:06:41 +0100

    naming and hover color fix

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -22,7 +22,7 @@ function navbarLinksRender() {
 }
 
 export default function Header() {
-	const [isMobileNavOpen, setIsMobileNavOpen] = useState(true);
+	const [isMobileNavCollapsed, setIsMobileNavCollapsed] = useState(true);
 	return (
 		<>
 			<header
@@ -32,7 +32,7 @@ export default function Header() {
 				<div className="grid grid-cols-[max-content_1fr] align-items-center col-center-content">
 					<p
 						className="mr-4 flex items-center text-3xl uppercase font-industrial font-medium tracking-widest md:tracking-wide lg:tracking-widest"
-						onClick={() => setIsMobileNavOpen(true)}
+						onClick={() => setIsMobileNavCollapsed(true)}
 					>
 						<Link
 							href="/"
@@ -42,27 +42,27 @@ export default function Header() {
 						</Link>
 					</p>
 					<div className="place-self-center justify-self-end md:hidden">
-						<div className="grid [grid-template-areas:'stack'] items-center text-2xl duration-100 hover:text-turquoise">
+						<div className={"grid [grid-template-areas:'stack'] items-center text-2xl duration-100" + (isMobileNavCollapsed ? " hover:text-turquoise" : "")}>
 							<Bars
-								className={"[grid-area:stack] cursor-pointer duration-500 " + (isMobileNavOpen ? "" : "opacity-0 rotate-[360deg] -z-1 blur-sm")}
-								onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}
+								className={"[grid-area:stack] cursor-pointer duration-500 " + (isMobileNavCollapsed ? "" : "opacity-0 rotate-[360deg] -z-1 blur-sm")}
+								onClick={() => setIsMobileNavCollapsed(!isMobileNavCollapsed)}
 							/>
 							<Xmark
-								className={"[grid-area:stack] cursor-pointer duration-500 " + (isMobileNavOpen ? "opacity-0 rotate-[-360deg] -z-1 blur-sm" : "")}
-								onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}
+								className={"[grid-area:stack] cursor-pointer duration-500 " + (isMobileNavCollapsed ? "opacity-0 rotate-[-360deg] -z-1 blur-sm" : "")}
+								onClick={() => setIsMobileNavCollapsed(!isMobileNavCollapsed)}
 							/>
 						</div>
 					</div>
 					<div
 						className={
-							(isMobileNavOpen ? "max-h-0 overflow-hidden " : "max-h-screen my-2 duration-500 ") +
+							(isMobileNavCollapsed ? "max-h-0 overflow-hidden " : "max-h-screen my-2 duration-500 ") +
 							" " +
 							"md:max-h-full w-full md:w-auto grid grid-flow-col gap-x-4 md:gap-x-0 col-span-2 md:col-span-1 justify-self-center md:justify-self-end justify-around md:m-0 font-systemui"
 						}
 					>
 						<ul
-							className={(isMobileNavOpen ? "" : "grid-flow-row ") + " md:max-h-full md:grid md:grid-flow-col gap-x-2 place-self-center"}
-							onClick={() => setIsMobileNavOpen(true)}
+							className={(isMobileNavCollapsed ? "" : "grid-flow-row ") + " md:max-h-full md:grid md:grid-flow-col gap-x-2 place-self-center"}
+							onClick={() => setIsMobileNavCollapsed(true)}
 						>
 							{navbarLinksRender()}
 						</ul>
@@ -71,7 +71,7 @@ export default function Header() {
 								<li key={socialItem.name}>
 									<Link
 										{...socialItem}
-										className={"hover:text-lion text-lg font-medium tracking-normal flex gap-x-1 items-center p-1 md:hidden " + (isMobileNavOpen ? " hidden " : "")}
+										className={"hover:text-lion text-lg font-medium tracking-normal flex gap-x-1 items-center p-1 md:hidden " + (isMobileNavCollapsed ? " hidden " : "")}
 									>
 										{socialItem.icon}
 										{socialItem.name}
