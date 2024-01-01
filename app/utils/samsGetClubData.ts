dd708c63570693db4c69f4dfc6957c91da47f496
Terijaki <590522+terijaki@users.noreply.github.com>
2024-01-01 21:33:16 +0100

    fetching with logging

--- app/utils/samsGetClubData.ts
+++ app/utils/samsGetClubData.ts
@@ -1,21 +1,36 @@
 // npx tsx --env-file=.env.local --env-file=.env app/utils/samsGetClubData.ts
+// http://wiki.sams-server.de/wiki/XML-Schnittstelle
 import { env } from "process";
+import fs from "fs";
 
-const samsApi = env.SAMS_API;
-const samsUrl = env.SAMS_URL;
-const samsClubId = env.SAMS_CLUBID;
+const SAMS_API = env.SAMS_API;
+const SAMS_URL = env.SAMS_URL;
+const SAMS_CLUB_ID = env.SAMS_CLUBID;
+const JSON_FILE_TARGET = "data/sams/club.json";
 
-console.log("hello");
-
-// fet clubData
-const apiPath = samsUrl + "/xml/sportsclub.xhtml?apiKey=" + samsApi + "&sportsclubId=" + samsClubId;
-console.log(apiPath);
-
-fetch(apiPath).then((response) => {
-	// Our handler throws an error if the request did not succeed.
-	if (!response.ok) {
-		throw new Error(`HTTP error: ${response.status}`);
-	} else {
-		console.log(response);
-	}
-});
+// fetch Club Data
+const apiPath = SAMS_URL + "/xml/sportsclub.xhtml?apiKey=" + SAMS_API + "&sportsclubId=" + SAMS_CLUB_ID;
+fetch(apiPath)
+	.then((response) => Promise.all([response.status, response.text()]))
+	.then(([status, xmlData]) => {
+		console.log("STATUS RESPONSE CODE:" + status);
+		if (status == 200) {
+			if (xmlData.includes("<id>" + SAMS_CLUB_ID + "</id>")) {
+				const parseString = require("xml2js").parseString;
+				parseString(xmlData, function (err: any, result: any) {
+					if (!err) {
+						console.log("✅ All good. Writing response to JSON file.");
+						fs.writeFileSync(JSON_FILE_TARGET, JSON.stringify(result));
+					} else {
+						console.log("🚨 COULD NOT CONVERT XML TO JSON! 🚨");
+						console.log(err);
+					}
+				});
+			} else {
+				console.log("🚨 RESPONSE BODY DID NOT INCLUDE OUR CLUB ID.🚨\nTHIS COULD INDICATE A MAINTENANCE OR SERVER ISSUE.");
+				console.log(xmlData);
+			}
+		} else {
+			console.log("🚨 DID NOT RECEIVE A HTTP 200 RESPONSE! 🚨");
+		}
+	});
