29f028f820697c08a4ca3171a94a9f1ec02bc536
Terijaki <590522+terijaki@users.noreply.github.com>
2024-01-02 12:23:59 +0100

    progress updte

--- app/utils/samsJsonClubData.ts
+++ app/utils/samsJsonClubData.ts
@@ -2,14 +2,18 @@ import fs from "fs";
 
 const CLUB_SAMS_FILE = "data/sams/club.json";
 
-export function getTeamIds(idType: "id" | "uuid" | "seasonTeamId" = "id") {
+export function getTeamIds(idType: "id" | "uuid" | "seasonTeamId" = "id", leagueOnly: boolean = true) {
 	const clubdata = fs.readFileSync(CLUB_SAMS_FILE);
 	const clubDataObject = JSON.parse(clubdata.toString());
 	const teams = clubDataObject.sportsclub.teams.team;
 	let teamIds = new Array();
 	teams.forEach((team: { status: string; matchSeries: { type: string }; id: string; uuid: string; seasonTeamId: string }) => {
-		if (team.status == "ACTIVE" && team.matchSeries.type == "League") {
-			teamIds.push(team[idType]);
+		if (team.status == "ACTIVE") {
+			if (leagueOnly && team.matchSeries.type == "League") {
+				teamIds.push(team[idType]);
+			} else if (!leagueOnly) {
+				teamIds.push(team[idType]);
+			}
 		}
 	});
 	return teamIds;
--- app/utils/samsUpdateTasks.ts
+++ app/utils/samsUpdateTasks.ts
@@ -1,11 +1,39 @@
+import fs from "fs";
+import { getTeamIds, getUniqueMatchSeriesIds } from "@/app/utils/samsJsonClubData";
+import getRankings from "@/app/utils/samsGetRankings";
+import getMatchSeries from "@/app/utils/samsGetMatchSeries";
 import getOurMatches from "@/app/utils/samsGetOurMatches";
 import getClubData from "@/app/utils/samsGetClubData";
 import getOurRankings from "@/app/utils/samsGetOurRankings";
-import getMatchSeries from "./samsGetMatchSeries";
 
-// this is simply a sequence of code to be executed in order
+// this a sequence of code to be executed in order to keep our data in sync with SAMS while not hammering their API unnecessarily
 
-getMatchSeries();
+getMatchSeries(); // there is no rate limit in this request type! ✌️
 // getClubData();
 // getOurMatches();
 // getOurRankings();
+
+// get our Match Series IDs so that we can use them to filter requests
+const ourMatchSeries = getUniqueMatchSeriesIds(getTeamIds("id"));
+// read the big Match Series file
+const matchSeriesJsonFile = fs.readFileSync("data/sams/MatchSeries.json");
+const matchSeriesJson = JSON.parse(matchSeriesJsonFile.toString());
+// filter down to our ids only
+const matchSeriesJsonFiltered = matchSeriesJson.matchSeriesList.matchSeries.filter((series: { id: string | number }) => ourMatchSeries.includes(series.id));
+// identify which of our Match Series need to be updated
+matchSeriesJsonFiltered.map((series: { id: string | number; type: string; structureUpdated: string; resultsUpdated: string; name: string }) => {
+	// RANKINGS
+	const rankingsJsonFile = fs.readFileSync("data/sams/matchSeriesId/" + series.id + "/rankings.json");
+	const rankingsJson = JSON.parse(rankingsJsonFile.toString());
+	if (rankingsJson.rankings.matchSeries.resultsUpdated != series.resultsUpdated || rankingsJson.rankings.matchSeries.structureUpdated != series.structureUpdated) {
+		console.log("Rankings for " + series.name + " are outdated. Fetching new rankings...");
+		getRankings(series.id);
+	}
+	// MATCHES
+	const matchesJsonFile = fs.readFileSync("data/sams/matchSeriesId/" + series.id + "/matches.json");
+	const matchesJson = JSON.parse(matchesJsonFile.toString());
+	if (matchesJson.matches.match[0].matchSeries.resultsUpdated != series.resultsUpdated || matchesJson.matches.match[0].matchSeries.structureUpdated != series.structureUpdated) {
+		console.log("Matches for " + series.name + " are outdated. Fetching new matches...");
+		getRankings(series.id);
+	}
+});
