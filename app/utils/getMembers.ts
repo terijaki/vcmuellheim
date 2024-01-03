b6c231a2a3ee446d805936d677d302c033da5a18
Terijaki <590522+terijaki@users.noreply.github.com>
2024-01-03 20:32:35 +0100

    optional fetch all members

--- app/utils/getMembers.ts
+++ app/utils/getMembers.ts
@@ -11,28 +11,46 @@ export type memberObject = {
 	avatar?: string;
 	function?: string;
 	sortorder?: number;
+	memberType?: string;
 };
 
-export function getMembers(memberType: "board" | "trainers", random?: false | true): memberObject[] {
-	const targetFolder = path.join(MEMBERS_FOLDER, memberType);
-	const membersFiles = fs.readdirSync(targetFolder);
-	const membersMatter = membersFiles.map((member) => {
-		const { data: frontmatter } = matter.read(path.join(targetFolder, member));
-		return frontmatter;
+export function getMembers(memberType?: "board" | "trainers", random?: false | true): memberObject[] {
+	// get the directories
+	const folders = fs.readdirSync(MEMBERS_FOLDER, { withFileTypes: true }).filter((dirent) => dirent.isDirectory());
+	let membersMatter: memberObject[] = [];
+	// read directories to get the files
+	folders.map((folder) => {
+		const files = fs.readdirSync(path.join(folder.path, folder.name));
+		// turn every file into an Object & add it to the membersMatter array
+		files.forEach((file) => {
+			const { data: frontmatter } = matter.read(path.join(folder.path, folder.name, file));
+			if (!frontmatter.sortorder) {
+				frontmatter.sortorder = 9999;
+			}
+			frontmatter.memberType = folder.name;
+			membersMatter.push(frontmatter);
+		});
 	});
+
+	// filter if there is a memberType specified
+	if (memberType) {
+		membersMatter = membersMatter.filter((member) => member.memberType == memberType);
+	}
+
+	// sort members if random is set to false
 	let membersSorted = membersMatter;
 	if (!random) {
 		membersSorted = membersMatter.sort((a, b) => {
 			if (!a.sortorder) {
-				a.sortorder = "9999";
+				a.sortorder = 9999;
 			}
 			if (!b.sortorder) {
-				b.sortorder = "9999";
+				b.sortorder = 9999;
 			}
 			return a.sortorder - b.sortorder;
 		});
 	} else {
-		membersSorted = shuffleArray(membersSorted);
+		membersSorted = shuffleArray(membersMatter);
 	}
 	return membersSorted;
 }
