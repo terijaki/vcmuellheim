0bfd8f47e7a15e50da6c8862669b51f876a833d9
Terijaki <590522+terijaki@users.noreply.github.com>
2024-01-03 14:09:16 +0100

    fixed location and all URL

--- app/utils/icsGeneration.ts
+++ app/utils/icsGeneration.ts
@@ -44,7 +44,10 @@ export function icsTeamGeneration(sbvvId: (string | number)[], slug: string) {
 			);
 			matchConstruct = matchConstruct.replaceAll(
 				"REPLACE_MATCH_DESCRIPTION",
-				match.matchSeries.name + (match.results && "\\nErgebnis: " + match.results.setPoints) + (match.host?.name && "\\nGastgeber: " + match.host.name) + "\\nhttps://vcmuellheim.de/teams/" + slug
+				match.matchSeries.name +
+					(match.results && match.results.setPoints && "\\nErgebnis: " + match.results.setPoints) +
+					(match.host?.name && "\\nGastgeber: " + match.host.name) +
+					(slug == "all" ? "\\nhttps://vcmuellheim.de/termine" : "\\nhttps://vcmuellheim.de/teams/" + slug)
 			);
 			matchArray.push(matchConstruct);
 		}
