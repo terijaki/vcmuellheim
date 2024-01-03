3544dd6af31f8708b7a006dd82ee6e1161dace94
Terijaki <590522+terijaki@users.noreply.github.com>
2024-01-03 12:31:16 +0100

    events as link

--- app/utils/navbarLinks.ts
+++ app/utils/navbarLinks.ts
@@ -2,6 +2,7 @@ export const navbarLinks = [
 	{ name: "News", href: "/news" },
 	{ name: "Mannschaften", href: "/#mannschaften" },
 	{ name: "Tabelle", href: "/tabelle" },
+	{ name: "Termine", href: "/termine" },
 	{ name: "Fotos", href: "/fotos" },
 	{ name: "Shop", href: "https://vcmuellheim.fan12.de", target: "_blank", rel: "noreferrer" },
 	{ name: "Kontakt", href: "./#kontakt" },
