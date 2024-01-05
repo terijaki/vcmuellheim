58c67a61fb5c690b27e427b48e926b2ff475b3f0
Terijaki <590522+terijaki@users.noreply.github.com>
2024-01-05 10:35:34 +0100

    added script, error logging and return

--- app/utils/samsUpdateTasks.ts
+++ app/utils/samsUpdateTasks.ts
@@ -8,48 +8,55 @@ import getClubData from "@/app/utils/samsGetClubData";
 import getMatches from "@/app/utils/samsGetMatches";
 
 // there is no rate limit in this request type! ✌️
-getMatchSeries().then(() => {
-	getClubData().then(() => {
-		// get our Match Series IDs so that we can use them to filter requests
-		const ourMatchSeries = getUniqueMatchSeriesIds(getTeamIds("id"));
-		// read the big Match Series file
-		const matchSeriesJsonFile = fs.readFileSync("data/sams/matchSeries.json");
-		const matchSeriesJson = JSON.parse(matchSeriesJsonFile.toString());
-		// filter down to our ids only
-		const matchSeriesJsonFiltered = matchSeriesJson.matchSeriesList.matchSeries.filter((series: { id: string | number }) => ourMatchSeries.includes(series.id.toString()));
-		// identify which of our Match Series need to be updated
-		matchSeriesJsonFiltered.map((series: { id: string | number; type: string; structureUpdated: string; resultsUpdated: string; name: string }) => {
-			// RANKINGS
-			const rankingsJsonFile = "data/sams/matchSeriesId/" + series.id + "/rankings.json";
-			if (fs.existsSync(rankingsJsonFile)) {
-				const rankingsJsonFileContent = fs.readFileSync(rankingsJsonFile);
-				const rankingsJson = JSON.parse(rankingsJsonFileContent.toString());
-				if (rankingsJson.rankings.matchSeries.resultsUpdated != series.resultsUpdated || rankingsJson.rankings.matchSeries.structureUpdated != series.structureUpdated) {
-					console.log("📋 Rankings for " + series.name + " (" + series.id + ") are outdated. Fetching new rankings...");
+getMatchSeries()
+	.then(() => {
+		getClubData().then(() => {
+			// get our Match Series IDs so that we can use them to filter requests
+			const ourMatchSeries = getUniqueMatchSeriesIds(getTeamIds("id"));
+			// read the big Match Series file
+			const matchSeriesJsonFile = fs.readFileSync("data/sams/matchSeries.json");
+			const matchSeriesJson = JSON.parse(matchSeriesJsonFile.toString());
+			// filter down to our ids only
+			const matchSeriesJsonFiltered = matchSeriesJson.matchSeriesList.matchSeries.filter((series: { id: string | number }) => ourMatchSeries.includes(series.id.toString()));
+			// identify which of our Match Series need to be updated
+			matchSeriesJsonFiltered.map((series: { id: string | number; type: string; structureUpdated: string; resultsUpdated: string; name: string }) => {
+				// RANKINGS
+				const rankingsJsonFile = "data/sams/matchSeriesId/" + series.id + "/rankings.json";
+				if (fs.existsSync(rankingsJsonFile)) {
+					const rankingsJsonFileContent = fs.readFileSync(rankingsJsonFile);
+					const rankingsJson = JSON.parse(rankingsJsonFileContent.toString());
+					if (rankingsJson.rankings.matchSeries.resultsUpdated != series.resultsUpdated || rankingsJson.rankings.matchSeries.structureUpdated != series.structureUpdated) {
+						console.log("📋 Rankings for " + series.name + " (" + series.id + ") are outdated. Fetching new rankings...");
+						getRankings(series.id);
+					} else {
+						console.log("✅ Rankings for " + series.name + " (" + series.id + ") are up to date.");
+					}
+				} else {
+					console.log("📋 Rankings for " + series.name + " (" + series.id + ") do not exist. Fetching new rankings...");
 					getRankings(series.id);
-				} else {
-					console.log("✅ Rankings for " + series.name + " (" + series.id + ") are up to date.");
 				}
-			} else {
-				console.log("📋 Rankings for " + series.name + " (" + series.id + ") do not exist. Fetching new rankings...");
-				getRankings(series.id);
-			}
-			// MATCHES
-			// need to check if the file already exists
-			const matchesJsonFile = "data/sams/matchSeriesId/" + series.id + "/matches.json";
-			if (fs.existsSync(matchesJsonFile)) {
-				const matchesJsonFileContent = fs.readFileSync(matchesJsonFile);
-				const matchesJson = JSON.parse(matchesJsonFileContent.toString());
-				if (matchesJson.matches.match[0].matchSeries.resultsUpdated != series.resultsUpdated || matchesJson.matches.match[0].matchSeries.structureUpdated != series.structureUpdated) {
-					console.log("📋 Matches for " + series.name + " (" + series.id + ") are outdated. Fetching new matches...");
+				// MATCHES
+				// need to check if the file already exists
+				const matchesJsonFile = "data/sams/matchSeriesId/" + series.id + "/matches.json";
+				if (fs.existsSync(matchesJsonFile)) {
+					const matchesJsonFileContent = fs.readFileSync(matchesJsonFile);
+					const matchesJson = JSON.parse(matchesJsonFileContent.toString());
+					if (matchesJson.matches.match[0].matchSeries.resultsUpdated != series.resultsUpdated || matchesJson.matches.match[0].matchSeries.structureUpdated != series.structureUpdated) {
+						console.log("📋 Matches for " + series.name + " (" + series.id + ") are outdated. Fetching new matches...");
+						getMatches(undefined, series.id);
+					} else {
+						console.log("✅ Matches for " + series.name + " (" + series.id + ") are up to date.");
+					}
+				} else {
+					console.log("📋 Matches for " + series.name + " (" + series.id + ") do not exist. Fetching new matches...");
 					getMatches(undefined, series.id);
-				} else {
-					console.log("✅ Matches for " + series.name + " (" + series.id + ") are up to date.");
 				}
-			} else {
-				console.log("📋 Matches for " + series.name + " (" + series.id + ") do not exist. Fetching new matches...");
-				getMatches(undefined, series.id);
-			}
+			});
 		});
+	})
+	.catch((error) => {
+		console.log(error);
+	})
+	.finally(() => {
+		return;
 	});
-});
