023c1b8cbef332545735b088eb5c91b2c22a66fd
Terijaki <590522+terijaki@users.noreply.github.com>
2024-01-06 12:06:29 +0100

    all event page

--- app/termine/page.tsx
+++ app/termine/page.tsx
@@ -18,12 +18,39 @@ export async function generateMetadata({}, parent: ResolvingMetadata): Promise<M
 export default function Termine() {
 	icsAllGeneration(); // triggers the generation of the all.ics file
 
-	if ((getMatches(getTeamIds("id"), "future").length = 0)) {
+	if (getMatches(getTeamIds("id"), "future").length > 0) {
+		return (
+			<>
+				<PageHeading title="Spielermine unserer Mannschaften" />
+				<div className="col-full-content sm:col-center-content card my-6">
+					<h2 className="card-heading">Vereinskalender</h2>
+					<p className="my-3 text-pretty">
+						<Link
+							href={"webcal://" + env.BASE_URL + "/ics/all.ics"}
+							className="gap-1 hyperlink group"
+						>
+							<IconSubscribe className="inline align-baseline" /> Abboniere unseren Vereinskalender
+						</Link>
+						, um neue Termine saisonübergreifend automatisch in deiner <span className="font-bold">Kalender-App</span> zu empfangen.
+					</p>
+				</div>
+				<div className="col-full-content sm:col-center-content card-narrow mb-6">
+					<h2 className="card-heading">Termine</h2>
+					<Matches
+						teamId={getTeamIds("id")}
+						filter="future"
+					/>
+				</div>
+			</>
+		);
+	} else {
+		let currentMonth = new Date().getMonth() + 1;
 		return (
 			<>
 				<PageHeading title="Spielermine unserer Mannschaften" />
 				<div className="col-full-content sm:col-center-content card my-6">
 					<p className="mb-3">Derzeit stehen keine weiteren Spieltermine an.</p>
+					{currentMonth >= 1 && currentMonth <= 9 ? <p className="mb-6">Die Saison im Hallenvolleyball findet in der Regel in den Monaten von September bis April statt.</p> : ""}
 					<p className="text-pretty">
 						<Link
 							href={"webcal://" + env.BASE_URL + "/ics/all.ics"}
@@ -37,29 +64,4 @@ export default function Termine() {
 			</>
 		);
 	}
-
-	return (
-		<>
-			<PageHeading title="Spielermine unserer Mannschaften" />
-			<div className="col-full-content sm:col-center-content card my-6">
-				<h2 className="card-heading">Vereinskalender</h2>
-				<p className="my-3 text-pretty">
-					<Link
-						href={"webcal://" + env.BASE_URL + "/ics/all.ics"}
-						className="gap-1 hyperlink group"
-					>
-						<IconSubscribe className="inline align-baseline" /> Abboniere unseren Vereinskalender
-					</Link>
-					, um neue Termine saisonübergreifend automatisch in deiner <span className="font-bold">Kalender-App</span> zu empfangen.
-				</p>
-			</div>
-			<div className="col-full-content sm:col-center-content card-narrow mb-6">
-				<h2 className="card-heading">Termine</h2>
-				<Matches
-					teamId={getTeamIds("id")}
-					filter="future"
-				/>
-			</div>
-		</>
-	);
 }
