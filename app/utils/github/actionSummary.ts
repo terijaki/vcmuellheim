6b31dfae21cacfac404a86a66d259408411e8f70
Terijaki <590522+terijaki@users.noreply.github.com>
2024-01-14 13:40:35 +0100

    Update actionSummary.ts

--- app/utils/github/actionSummary.ts
+++ app/utils/github/actionSummary.ts
@@ -5,7 +5,7 @@ const GITHUB_SUMMARY_FILE = "github_summary.md";
 // this writes to a file that is being read during a github actions run to craft a summary of the job
 export function writeToSummary(text: string) {
 	if (!fs.existsSync(GITHUB_SUMMARY_FILE)) {
-		fs.writeFileSync(GITHUB_SUMMARY_FILE, "");
+		fs.writeFileSync(GITHUB_SUMMARY_FILE, " ");
 	}
 	fs.appendFileSync(GITHUB_SUMMARY_FILE, text + "\n");
 }
