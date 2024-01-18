16bc7441fb67f39d9a1de0102fdc7fcc1d8d315e
Terijaki <590522+terijaki@users.noreply.github.com>
2024-01-18 18:29:16 +0100

    refactor inbox automation Fixes #132

--- app/utils/inboxProcessing.ts
+++ app/utils/inboxProcessing.ts
@@ -3,9 +3,10 @@ import path from "path";
 import { writeToSummary } from "./github/actionSummary";
 
 const INBOX_FOLDER = "inbox",
-	IMAGE_FOLDER = "images/blog",
+	IMAGE_FOLDER = "public/images/blog",
 	DOCUMENT_EXTENSIONS = ["docx", "rtf", "txt", "odt"],
-	IMAGE_EXTENSIONS = ["jpg", "jpeg", "png"];
+	IMAGE_EXTENSIONS = ["jpg", "jpeg", "png"],
+	POST_TARGET_FOLDER = "data/posts";
 
 export function inboxProcessing() {
 	// check if the inbox directory is even present
@@ -15,8 +16,6 @@ export function inboxProcessing() {
 		files.forEach((file) => {
 			message = message + "\n- " + file;
 		});
-		// console.log(message);
-		// writeToSummary(message);
 
 		// identify documents
 		const documents = files.filter((element) => {
@@ -25,31 +24,62 @@ export function inboxProcessing() {
 				return true;
 			}
 		});
-		console.log("Number of documents found: " + documents.length);
 
 		// identify images, rename & move them and note them down as reference
-		let images = new Set();
+		let images = new Set<string[]>();
 		files.forEach((file) => {
 			const thisExtension = file.split(".").pop() + "";
 			if (IMAGE_EXTENSIONS.includes(thisExtension.toLowerCase())) {
 				const newName = file.toLowerCase().replace(".jpeg", ".jpg");
 				const dateSubfolders = new Date().toISOString().slice(0, 10).replaceAll("-", "/");
-				const newDirectory = path.join(IMAGE_FOLDER, new Date().toISOString().slice(0, 10).replaceAll("-", "/"));
+				const newDirectory = path.join(IMAGE_FOLDER, dateSubfolders);
 				const newPath = path.join(newDirectory, newName);
-				console.log(newPath);
-				// fs.renameSync(path.join(INBOX_FOLDER, file), newPath); TODO uncomment this to actually remove the image
-				images.add(newPath);
+				const oldPath = path.join(INBOX_FOLDER, file);
+				images.add([oldPath, newPath]);
 			}
 		});
 
-		// NEXT:
-		// extract the text
-		// create frontmatter
-		// create gallery
-		// construct file name (prefix is new Date().toISOString().slice(0, 10))
-		// write to .md file at the target destination
+		// check conditions | either a single post with images or multiple posts without images are allowed
+		if (documents.length > 1 && images.size > 0) {
+			throw "🚨 " + images.size + " images and " + documents.length + " document found in the inbox. This utility can only process one document at a time while images are present!";
+		} else {
+			documents.forEach(async (document) => {
+				// isolate the file name as title
+				const fileNameLenght = document.lastIndexOf(".");
+				const title = document.slice(0, fileNameLenght);
+				// read the document
+				const WordExtractor = require("word-extractor");
+				const extractor = new WordExtractor();
+				const extracted = await extractor.extract(path.join(INBOX_FOLDER, document));
+				const content = extracted.getBody();
+				const markdownDate = new Date().toISOString();
+				let markdownGallery = "";
+				if (images.size > 0) {
+					markdownGallery = "gallery:\n";
+					images.forEach((image) => {
+						markdownGallery = markdownGallery + "- /" + image[1].replace("public/", "") + "\n"; // here the nextJS public folder needs to be omitted
+					});
+				}
+				// patch the markdown together
+				const markdownContent = "---\n" + "title: '" + title + "'\n" + "date: " + markdownDate + "\n" + markdownGallery + "---\n" + content;
+				// write the markdown file
+				const documentTargetPath = POST_TARGET_FOLDER + "/" + new Date().toISOString().slice(0, 10) + "-" + title.toLowerCase().replaceAll(" ", "-") + ".md";
+				fs.writeFileSync(documentTargetPath, markdownContent);
+				images.forEach((image) => {
+					const newFolder = image[1].slice(0, image[1].lastIndexOf("/"));
+					fs.mkdirSync(newFolder, { recursive: true });
+					fs.renameSync(image[0], image[1]); // moves the image from its old location to the new location
+				});
+				fs.rmSync(path.join(INBOX_FOLDER, document));
+				let message = "✅ Processed " + documents.length + " blog post: " + '"' + title + '"';
+				if (images.size > 0) {
+					message = message.replace("blog post:", "blog post with " + images.size + " images:");
+				}
+				writeToSummary(message);
+				console.log(message);
+			});
+		}
 	}
-	// npx tsx app/utils/inboxProcessing.ts
 }
 
 inboxProcessing();
