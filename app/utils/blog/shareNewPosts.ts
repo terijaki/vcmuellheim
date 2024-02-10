fda13d7e86d63f9df934fca2722ade81bf963348
Terijaki <590522+terijaki@users.noreply.github.com>
2024-02-10 20:24:46 +0100

    finalized function

--- app/utils/blog/shareNewPosts.ts
+++ app/utils/blog/shareNewPosts.ts
@@ -1,6 +1,9 @@
 import fs from "fs";
 import matter from "gray-matter";
 import path from "path";
+import { env } from "process";
+import { mastodonPostStatus, mastodonSearchStatus } from "../social/mastodon";
+import { writeToSummary } from "../github/actionSummary";
 
 const POST_PATH = "data/posts",
 	TIME_TOLERANCE = 7; // exclude any older posts than this value (in days)
@@ -13,6 +16,7 @@ export type postFrontmatter = {
 		date?: string;
 		gallery?: string[];
 		mastodon?: shareStatus;
+		mastodonUrl?: string;
 	};
 	path?: string;
 };
@@ -36,29 +40,71 @@ export function shareNewPosts() {
 				}
 			}
 		});
-		console.log(newPosts);
+		// console.log(newPosts);
 
-		newPosts.forEach((content) => {
-			content.data.mastodon = "queued";
-			console.log(matter.stringify(content));
-			// https://www.npmjs.com/package/gray-matter
-			// console.log(matter.stringify('foo bar baz', {title: 'Home'}));
-			// attempt to share and write queued status
-			// share
-			// write published status
-			// mastodon: new | queued | published
+		newPosts.forEach(async (content) => {
+			if (!content.path) {
+				throw "🚨 Object does not contain path! Therefore cannot modify the post's file.";
+			} else {
+				if (!env.BASE_URL) {
+					throw "🚨 Environmental variable BASE_URL is missing!";
+				} else {
+					// set status to queued before attempting to share
+					content.data.mastodon = "queued";
+					const contentAsString = matter.stringify(content, {}); // transform it back to a string so that it can be writen back to the file
+					fs.writeFileSync(content.path, contentAsString); // update the post file
+
+					// construct the URL to be shared
+					let postUrl = content.path.replace(POST_PATH, ""); // remove the path
+					postUrl = postUrl.slice(0, postUrl.lastIndexOf(".")); // remove the file extension
+					postUrl = "https://" + env.BASE_URL + postUrl;
+					const message: string = content.data.title + " " + postUrl;
+
+					// check if the post has already been shared to avoid duplicates
+					const checkStatusExistance = await mastodonSearchStatus(postUrl);
+					if (checkStatusExistance.status != 200) {
+						throw "Could not verify status existence:" + checkStatusExistance.status;
+					} else {
+						checkStatusExistance.response.map((response) => {
+							// do not post duplicate if a status is found
+							if (response.created_at) {
+								let consoleNote =
+									"🚨 Message has already been shared on " + new Date(response.created_at).toLocaleString("en-GB", { dateStyle: "short" }) + " (" + response.url + "). Marking post as published.";
+								console.log(consoleNote);
+								writeToSummary(consoleNote);
+								// set frontmatter to published
+								content.data.mastodon = "published";
+								content.data.mastodonUrl = response.url;
+							}
+						});
+
+						if (!content.data.mastodonUrl) {
+							console.log('🕵️ attempting to share to Mastodon: "' + message + '"');
+							// share the message as Mastodon status
+							const postStatus = await mastodonPostStatus(message); // this will post the message
+
+							if (postStatus.status == 200) {
+								let consoleNote = "✅ Post shared successfully: " + postStatus.response.url;
+								console.log(consoleNote);
+								writeToSummary(consoleNote);
+								// set frontmatter to published
+								content.data.mastodon = "published";
+								content.data.mastodonUrl = postStatus.response.url;
+							} else {
+								let consoleNote = "🚨 Post (" + content.data.title + ") could not be shared! " + postStatus.status;
+								console.log(consoleNote);
+								writeToSummary(consoleNote);
+								throw consoleNote;
+							}
+						}
+						const updatedContentAsString = matter.stringify(content, {}); // transform the content Object to a string so that it can be writen back to the file
+						fs.writeFileSync(content.path, updatedContentAsString); // update the post file
+					}
+				}
+			}
 		});
 	}
-	// forEach:
-	// read frontmatter
-	// filter by Date
-	// filter by not-yet-shared
-	// share
-	// write published status
-	// mastodon: new | queued | published
-
 	return true;
 }
 
 shareNewPosts();
-// npm run blog-new-posts
