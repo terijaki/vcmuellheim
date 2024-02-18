07e84dae97bf42491706a5c5cde3980f26a4eedb
Terijaki <590522+terijaki@users.noreply.github.com>
2024-02-18 17:57:24 +0100

    remote club logos

---
+++ app/utils/sams/listRemoteClubLogos.ts
@@ -0,0 +1,22 @@
+import fs from "fs";
+import path from "path";
+
+const CLUBS_CACHE_FOLDER = "data/sams/clubs";
+
+// Remote logo images  should be cache during and build time and to do so next-image-export-optimizer requires a list of all remote images to be added to remoteOptimizedImages.js
+export default function listRemoteClubLogos(): string[] | undefined {
+	if (fs.existsSync(CLUBS_CACHE_FOLDER)) {
+		let clubImages = new Set<string>();
+
+		const clubs = fs.readdirSync(CLUBS_CACHE_FOLDER);
+		clubs.map((clubFile) => {
+			const clubFileContent = fs.readFileSync(path.join(CLUBS_CACHE_FOLDER, clubFile));
+			const clubData = JSON.parse(clubFileContent.toString());
+
+			if (clubData.logo) {
+				clubImages.add(clubData.logo);
+			}
+		});
+		return Array.from(clubImages);
+	}
+}
