4843b1c852933af721a612f762d824328fb01751
Terijaki <590522+terijaki@users.noreply.github.com>
2024-02-18 18:24:13 +0100

    syntax refert

--- remoteOptimizedImages.js
+++ remoteOptimizedImages.js
@@ -1,6 +1,6 @@
 //In order for the image optimization at build time to work correctly, you have to specify all remote image urls in a file called remoteOptimizedImages.js in the root directory of your project (where the next.config.js is stored as well). The file should export an array of strings containing the urls of the remote images. Returning a promise of such array is also supported.
 
-export const logos = new Promise(async (resolve) => {
+module.exports = new Promise(async (resolve) => {
 	const listRemoteClubLogos = require("./app/utils/sams/listRemoteClubLogos.ts");
 	let logoArray = await listRemoteClubLogos();
 	if (!logoArray) {
