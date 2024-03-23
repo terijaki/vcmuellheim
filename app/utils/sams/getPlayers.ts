31624edfe488a05f20b0ae032b93151ea8829c6a
Terijaki <590522+terijaki@users.noreply.github.com>
2024-03-23 09:32:07 +0100

    new function

---
+++ app/utils/sams/getPlayers.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+import path from "path";
+
+const SAMS_PLAYER_URL = "https://www.sbvv-online.de/servlet/sportsclub/TeamMemberCsvExport?teamId=";
+const TEAM_FOLDER = "data/sams/team";
+
+export default async function getPlayers(teamId: number | string) {
+	const dataUrl: string = SAMS_PLAYER_URL + teamId;
+
+	const res = await fetch(dataUrl);
+	if (res.ok) {
+		// read the player data from SAMS
+		const samsData = await res.text();
+		// convert it to json
+		const dumbcsv = require("dumb-csv");
+		const jsonData = await dumbcsv.fromCSV({ data: samsData, separator: ";" }).toJSON();
+		// clean the json - only return wanted data and rename it
+		const jsonClean = jsonData.map((player: { [x: string]: any }) => {
+			return {
+				lastname: player["Nachname"],
+				firstname: player["Vorname"],
+				nationality: player["NAT"],
+				number: player["Trikot"],
+				height: player["Gr��e"],
+				function: player["Position/Funktion Offizieller"],
+			};
+		});
+		// store the player data
+		const targetPath = path.join(TEAM_FOLDER, teamId.toString());
+		fs.mkdirSync(targetPath, { recursive: true });
+		const targetFile = path.join(targetPath, "players.json");
+		fs.writeFileSync(targetFile, JSON.stringify(jsonClean, null, 4));
+	}
+}
