d54bfc4941a8e923157604bf14d74b904250721e
Terijaki <590522+terijaki@users.noreply.github.com>
2024-07-05 09:07:30 +0200

    tweaks

--- app/components/sams/ClubLogo.tsx
+++ app/components/sams/ClubLogo.tsx
@@ -1,19 +1,8 @@
-"use client";
 import Image from "next/image";
-import { useState } from "react";
 import { FaVolleyball as Ball } from "react-icons/fa6";
 
 export default async function clubLogo(props: { clubName: string; logo?: string; className?: string }) {
-	const [loadError, setLoadError] = useState(false);
-
 	if (props.logo) {
-		if (loadError)
-			return (
-				<div className={"inline-flex h-5 w-5 text-lion items-center justify-center " + props.className}>
-					<Ball className="p-0.3 opacity-50" />
-				</div>
-			);
-
 		return (
 			<div className={"inline-block relative h-5 w-5 " + props.className}>
 				<Image
@@ -23,7 +12,6 @@ export default async function clubLogo(props: { clubName: string; logo?: string;
 					placeholder="empty"
 					alt={"Logo: " + props.clubName}
 					className="object-contain mix-blend-multiply"
-					onError={() => setLoadError(true)}
 				/>
 			</div>
 		);
