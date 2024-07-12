1888400af77e42749d9263e68f3ed5edb264f800
Terijaki <590522+terijaki@users.noreply.github.com>
2024-07-12 08:22:22 +0200

    fake url during dev

--- project.config.ts
+++ project.config.ts
@@ -1,7 +1,5 @@
 //
-//
 // This file contains variables which are required for several features across the project. They are influenced from external factors and they are put here to easily change these from a central location. They are not sensitive in any way and we are not using environmental variables here for this reason.
-//
 //
 /** General information about the club. */
 export const Club = {
@@ -12,8 +10,8 @@ export const Club = {
 	email: "info@vcmuellheim.de",
 };
 /** The clubs details on the SAMS platform. */
-export const SAMS = { 
-	url: "https://www.volleyball-baden.de",
+export const SAMS = {
+	url: samsUrl(),
 	vereinsnummer: 3530, // in 2024 SAMS united across federal states and since then this is the "internalSportsclubId" and no longer the "clubId"
 	name: "VC Müllheim",
 };
@@ -23,3 +21,8 @@ export const Mastodon = {
 	name: "VCM",
 	clientId: "109553572668731614",
 };
+
+function samsUrl() {
+	if (process.env.NODE_ENV == "production") return "https://www.volleyball-baden.de";
+	return "http://localhost:3080/sams/mockup";
+}
