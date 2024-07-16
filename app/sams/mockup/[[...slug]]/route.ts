5c1b27486c2df35ef0cefddb8c59615976300683
Terijaki <590522+terijaki@users.noreply.github.com>
2024-07-16 11:32:43 +0200

    cleanup

--- app/sams/mockup/[[...slug]]/route.ts
+++ app/sams/mockup/[[...slug]]/route.ts
@@ -1,18 +1,14 @@
 import { SAMS } from "@/project.config";
 import { type NextRequest } from "next/server";
-import fs from "fs";
-import { init } from "next/dist/compiled/webpack/webpack";
 
 export const revalidate = false;
 
 export async function GET(request: NextRequest) {
 	const requsetUrl = request.nextUrl;
 	const requsetUrlPath = request.nextUrl.pathname;
-	const searchParams = request.nextUrl.searchParams;
-	const query = searchParams.get("query");
+	// const searchParams = request.nextUrl.searchParams;
+	// const query = searchParams.get("query");
 	// query is "hello" for /api/search?query=hello
-	console.log(requsetUrl);
-	let context = requsetUrl.toString().replace(SAMS.url, "").replace("/xml", "");
 
 	let responseBody: {} = {
 		status: 400,
