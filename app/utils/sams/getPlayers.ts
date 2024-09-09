fc5cc413dbabb9a793dde47e1db0f0a4e59ed358
Terijaki <590522+terijaki@users.noreply.github.com>
2024-09-09 07:51:51 +0200

    fix player data fetching

--- app/utils/sams/getPlayers.ts
+++ app/utils/sams/getPlayers.ts
@@ -24,55 +24,58 @@ export default async function getPlayers(teamId: (number | string)[]) {
 	for (const id of teamId) {
 		const targetPath = path.join(TEAM_FOLDER, id.toString());
 		const targetFile = path.join(targetPath, "players.json");
+
+		let playerDataAge = new Date("1980-01-01"); // fake date in case the file does not exist
 		// check if the player data already exists and its not older than 3 days
 		if (fs.existsSync(targetFile)) {
 			const existingFile = fs.readFileSync(targetFile);
 			const existingJsonObject = JSON.parse(existingFile.toString());
-			// set the age of the cache file after it needs to be updated
-			const cacheTTL = 3,
-				today = new Date(),
-				cacheTTLDate = new Date(today.setDate(today.getDate() - cacheTTL)).getTime();
-			if (cacheTTLDate > new Date(existingJsonObject.date).getTime()) {
-				const dataUrl: string = SAMS_PLAYER_URL + id;
-				// receive the player data from SAMS
-				await fetch(dataUrl, { headers: { "Content-Type": "text/html; charset=UTF-8" }, cache: "force-cache", next: { revalidate: 600, tags: ["sams", "players"] } })
-					.then((response) => response.arrayBuffer())
-					.then(async (buffer) => {
-						let decoder = new TextDecoder("iso-8859-1"); // this is needed to deal with german Umlauts
-						let samsData = decoder.decode(buffer);
-						// debug messages
-						if (samsData) {
-							let consoleNote = "✅ Player data retrieved for team " + id;
-							console.log(consoleNote);
-							summaryList.add(consoleNote);
-						}
-						// convert it to json
-						const dumbcsv = require("dumb-csv");
-						const jsonPlayerData = await dumbcsv.fromCSV({ data: samsData, separator: ";" }).toJSON();
-						// build the object to be exported
-						let jsonData: playersType = { date: new Date() };
-						// add the relevant player data with simplified terminology
-						jsonData.players = jsonPlayerData.map((player: { [x: string]: any }) => {
-							return {
-								lastname: player["Nachname"],
-								firstname: player["Vorname"],
-								nationality: player["NAT"],
-								number: player["Trikot"],
-								height: player["Größe"],
-								function: player["Position/Funktion Offizieller"],
-							};
-						});
-						// store the player data
-						fs.mkdirSync(targetPath, { recursive: true });
-						fs.writeFileSync(targetFile, JSON.stringify(jsonData, null, 4));
-					})
-					.catch((error) => {
-						let consoleNote = "🚨 Player data for team " + teamId + " could not be processed correctly: " + error;
+			playerDataAge = existingJsonObject.date;
+		}
+		// set the age of the cache file after it needs to be updated
+		const cacheTTL = 3,
+			today = new Date(),
+			cacheTTLDate = new Date(today.setDate(today.getDate() - cacheTTL)).getTime();
+		if (cacheTTLDate > new Date(playerDataAge).getTime()) {
+			const dataUrl: string = SAMS_PLAYER_URL + id;
+			// receive the player data from SAMS
+			await fetch(dataUrl, { headers: { "Content-Type": "text/html; charset=UTF-8" }, cache: "force-cache", next: { revalidate: 600, tags: ["sams", "players"] } })
+				.then((response) => response.arrayBuffer())
+				.then(async (buffer) => {
+					let decoder = new TextDecoder("iso-8859-1"); // this is needed to deal with german Umlauts
+					let samsData = decoder.decode(buffer);
+					// debug messages
+					if (samsData) {
+						let consoleNote = "✅ Player data retrieved for team " + id;
 						console.log(consoleNote);
 						summaryList.add(consoleNote);
-						summaryErrorList.add(consoleNote);
+					}
+					// convert it to json
+					const dumbcsv = require("dumb-csv");
+					const jsonPlayerData = await dumbcsv.fromCSV({ data: samsData, separator: ";" }).toJSON();
+					// build the object to be exported
+					let jsonData: playersType = { date: new Date() };
+					// add the relevant player data with simplified terminology
+					jsonData.players = jsonPlayerData.map((player: { [x: string]: any }) => {
+						return {
+							lastname: player["Nachname"],
+							firstname: player["Vorname"],
+							nationality: player["NAT"],
+							number: player["Trikot"],
+							height: player["Größe"],
+							function: player["Position/Funktion Offizieller"],
+						};
 					});
-			}
+					// store the player data
+					fs.mkdirSync(targetPath, { recursive: true });
+					fs.writeFileSync(targetFile, JSON.stringify(jsonData, null, 4));
+				})
+				.catch((error) => {
+					let consoleNote = "🚨 Player data for team " + teamId + " could not be processed correctly: " + error;
+					console.log(consoleNote);
+					summaryList.add(consoleNote);
+					summaryErrorList.add(consoleNote);
+				});
 		}
 	}
 
