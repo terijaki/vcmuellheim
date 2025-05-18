e329b5167feb45904a6686da4395c1785bb32778
Terijaki <590522+terijaki@users.noreply.github.com>
2025-05-18 11:37:44 +0200

    attempt to fix undefined error

--- loader.js
+++ loader.js
@@ -1,12 +1,15 @@
 "use client";
 
+const COOLIFY_URL = process.env.COOLIFY_URL;
+
 export default function myImageLoader({ src, width, quality }) {
+  if (!COOLIFY_URL) return src;
   const isLocal = !src.startsWith("http");
   const query = new URLSearchParams();
 
   const imageOptimizationApi = "https://nit.terijaki.eu";
   // Your NextJS application URL
-  const baseUrl = `https://${process.env.COOLIFY_URL}`; // this is the domain configured in coolify
+  const baseUrl = `https://${COOLIFY_URL}`; // this is the domain configured in coolify
 
   const fullSrc = `${baseUrl}${src}`;
 
