004d58f133a864e0ca1afd7c541daf37987a3d40
Terijaki <590522+terijaki@users.noreply.github.com>
2025-05-31 07:39:08 +0200

    temporary force update

--- jobs/tasks/sams-club-update.ts
+++ jobs/tasks/sams-club-update.ts
@@ -2,7 +2,7 @@ import { samsTeamsUpdate } from "@/utils/sams/sams-server-actions";
 import { Cron } from "croner";
 
 export const samsClubUpdate = new Cron(
-	"0 3 * * *", // every day at 3 AM
+	"* * * * *", // every day at 3 AM
 	{
 		name: "Sams Club Update",
 		paused: true,
