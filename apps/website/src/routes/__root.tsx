ec5cbf43da30adc22bcc37cea2b1ae704b379fcd
Terijaki <590522+terijaki@users.noreply.github.com>
2025-11-27 12:25:11 +0100

    feat: implement bus route component with calendar and booking display; enhance layout with alerts and tables

--- apps/website/src/routes/__root.tsx
+++ apps/website/src/routes/__root.tsx
@@ -7,32 +7,28 @@ import PageWithHeading from "../components/layout/PageWithHeading";
 
 export const Route = createRootRoute({
 	component: () => (
-		<AppShell header={{ height: HEADER_HEIGHT, offset: true }} withBorder={false} bg="onyx">
-			<Header />
-			<AppShellMain bg="aquahaze">
-				<Stack justify="space-between" style={{ minHeight: `calc(100vh - ${HEADER_HEIGHT}px)` }}>
-					<Outlet />
-					<Footer />
-				</Stack>
-			</AppShellMain>
-		</AppShell>
+		<MainLayout>
+			<Outlet />
+		</MainLayout>
 	),
 	notFoundComponent: () => (
-		<PageWithHeading title="Seite nicht gefunden! 🫤">
-			<Container size="sm">
-				<Stack gap="xl">
-					<Card>
-						<Text>Die angeforderte Seite konnte nicht gefunden werden. Bitte überprüfe die URL ({window.location.pathname}) auf Tippfehler.</Text>
-						<Text>Falls du denkst, dass dies ein Fehler ist, kontaktiere uns gerne.</Text>
-					</Card>
-					<Center>
-						<Button component={Link} href="/">
-							Zurück zur Startseite
-						</Button>
-					</Center>
-				</Stack>
-			</Container>
-		</PageWithHeading>
+		<MainLayout>
+			<PageWithHeading title="Seite nicht gefunden! 🫤">
+				<Container size="sm">
+					<Stack gap="xl">
+						<Card>
+							<Text>Die angeforderte Seite konnte nicht gefunden werden. Bitte überprüfe die URL ({window.location.pathname}) auf Tippfehler.</Text>
+							<Text>Falls du denkst, dass dies ein Fehler ist, kontaktiere uns gerne.</Text>
+						</Card>
+						<Center>
+							<Button component={Link} href="/">
+								Zurück zur Startseite
+							</Button>
+						</Center>
+					</Stack>
+				</Container>
+			</PageWithHeading>
+		</MainLayout>
 	),
 	pendingComponent: () => (
 		<PageWithHeading title="">
@@ -43,23 +39,39 @@ export const Route = createRootRoute({
 		// TODO: Log error to Sentry
 		console.error(error);
 		return (
-			<PageWithHeading title="Hoppala! 🫢">
-				<Container size="sm">
-					<Stack gap="xl">
-						<Card>
-							<Text>Etwas ist schief gelaufen. Der Server konnte dir diesen Bereich ({window.location.pathname}) nicht fehlerfrei darstellen.</Text>
-							<Text>Bitte versuche es zu einem späteren Zeitpunkt noch einmal.</Text>
-						</Card>
-						<Center>
-							<Group>
-								<Button component={Link} href="/">
-									Zurück zur Startseite
-								</Button>
-							</Group>
-						</Center>
-					</Stack>
-				</Container>
-			</PageWithHeading>
+			<MainLayout>
+				<PageWithHeading title="Hoppala! 🫢">
+					<Container size="sm">
+						<Stack gap="xl">
+							<Card>
+								<Text>Etwas ist schief gelaufen. Der Server konnte dir diesen Bereich ({window.location.pathname}) nicht fehlerfrei darstellen.</Text>
+								<Text>Bitte versuche es zu einem späteren Zeitpunkt noch einmal.</Text>
+							</Card>
+							<Center>
+								<Group>
+									<Button component={Link} href="/">
+										Zurück zur Startseite
+									</Button>
+								</Group>
+							</Center>
+						</Stack>
+					</Container>
+				</PageWithHeading>
+			</MainLayout>
 		);
 	},
 });
+
+function MainLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<AppShell header={{ height: HEADER_HEIGHT, offset: true }} withBorder={false} bg="onyx">
+			<Header />
+			<AppShellMain bg="aquahaze">
+				<Stack justify="space-between" style={{ minHeight: `calc(100vh - ${HEADER_HEIGHT}px)` }}>
+					{children}
+					<Footer />
+				</Stack>
+			</AppShellMain>
+		</AppShell>
+	);
+}
