9e41d124f55474c61e8ef405c88913093df1bca1
Terijaki <590522+terijaki@users.noreply.github.com>
2025-12-02 11:59:43 +0100

    fix: add lazy loading to Image component in PictureCard for improved performance

--- apps/website/src/components/PictureCard.tsx
+++ apps/website/src/components/PictureCard.tsx
@@ -5,6 +5,7 @@ export default function PictureCard({ url, fixedHeight }: { url: string; fixedHe
 		<AspectRatio ratio={16 / 9} w={fixedHeight ? { base: "100%", xs: fixedHeight } : undefined} h={fixedHeight ? { base: undefined, xs: (fixedHeight / 16) * 9 } : undefined}>
 			<Card shadow="sm" component="a" href={url} target="_blank" rel="noopener noreferrer" p={0} radius="md">
 				<Image
+					loading="lazy"
 					src={url}
 					alt=""
 					style={{
