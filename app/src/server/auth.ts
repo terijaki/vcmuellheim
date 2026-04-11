fe056cdeb074008a5586c585c4c388c21cf026d1
Terijaki <590522+terijaki@users.noreply.github.com>
2026-04-11 14:41:03 +0200

    refactor: simplify getTrusedOrigins function by removing commented-out code

--- app/src/server/auth.ts
+++ app/src/server/auth.ts
@@ -64,10 +64,7 @@ function createOtpLoginLink(email: string, otp: string, request?: Request): stri
 }
 
 function getTrusedOrigins({ isLocalDev = false } = {}): string[] {
-	const origins = [
-		`https://${Club.domain}`,
-		// , "https://*.lambda-url.eu-central-1.on.aws" // TODO check if this is fine without
-	];
+	const origins = [`https://${Club.domain}`];
 
 	if (!isProd) {
 		origins.push(`https://*.new.${Club.domain}`);
